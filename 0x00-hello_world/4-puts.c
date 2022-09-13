9c2218cbdc53bea6884f794520f5f4ee4837db0d
Muchowkey <muchokibenson60@gmail.com>
2022-09-13 11:39:22 -0700

    added file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * 
+ * 
+ * The script in this file prints"\ "Programming is like building a multilingual puzzle"
+ * and Returns(0)
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}

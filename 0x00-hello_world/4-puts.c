0b43523bcdca978091ee47e102d8b1f683bf60e8
Muchowkey <muchokibenson60@gmail.com>
2022-09-14 12:47:36 -0700

    updated file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /*
  * Main-This is the entry point  
  * Description: The script in this file prints"\"Programming is like building a multilingual puzzle"
  * Returns: 0
- *
- *
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

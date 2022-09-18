bb8285ecff7dbc2b02fb28194d5390018fbe2dae
Muchowkey <muchokibenson60@gmail.com>
2022-09-18 06:10:49 -0700

    added file 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints all numbers of base 16
+ * Return 0
+ */
+
+int main(void)
+{
+	char a;
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	
+	putchar('\n');
+	return (0);
+}

1339761d932d231a69a112cd1178811d9ac1464e
Muchowkey <muchokibenson60@gmail.com>
2022-09-19 13:39:22 -0700

    house cleaning

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: Prints all lowercase letters
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char ltr = 'a';
+
+	while (ltr <= 'z');
+	{
+		_putchar(ltr);
+		ltr++;
+	}
+
+	_putchar('\n');
+	return (0);
+}

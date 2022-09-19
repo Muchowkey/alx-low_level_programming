be5e5c6fbf5960fe0bc53f7f8105427238a68b4a
Muchowkey <muchokibenson60@gmail.com>
2022-09-18 23:52:51 -0700

    added file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: The script prints _putchar followed by a new line
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
